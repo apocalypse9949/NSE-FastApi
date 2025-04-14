c95350e708db98f4a28f3bfe62284fcbdc2cecdd
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-04-14 15:29:12 +0530

    Update fastapi.py

--- fastapi.py
+++ fastapi.py
@@ -52,5 +52,5 @@ nest_asyncio.apply()
 try:
     uvicorn.run(app, host="0.0.0.0", port=8000)
 except KeyboardInterrupt:
-    print("🛑 Server manually stopped.")
+    print("Server manually stopped.")
 
